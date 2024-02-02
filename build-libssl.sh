f80fddebbafa29322fd3f49c0ccda3d18fa197aa
Georgi Valkov <gvalkov@gmail.com>
2024-02-02 11:46:45 +0200

    Add support for openssl-3.2.1
    Signed-off-by: Georgi Valkov <gvalkov@gmail.com>

--- build-libssl.sh
+++ build-libssl.sh
@@ -25,7 +25,7 @@ set -u
 # SCRIPT DEFAULTS
 
 # Default version in case no version is specified
-DEFAULTVERSION="3.1.2"
+DEFAULTVERSION="3.2.1"
 
 # Default (=full) set of targets to build
 DEFAULTTARGETS="darwin64-x86_64-cc ios64-cross"
