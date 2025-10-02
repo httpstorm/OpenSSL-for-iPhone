f7eab3ea7c1846b0b9ed41e6520b986174d37aee
Georgi Valkov <gvalkov@gmail.com>
2025-10-02 17:19:36 +0300

    Add support for openssl-3.6.0
    Signed-off-by: Georgi Valkov <gvalkov@gmail.com>

--- build-libssl.sh
+++ build-libssl.sh
@@ -25,7 +25,7 @@ set -u
 # SCRIPT DEFAULTS
 
 # Default version in case no version is specified
-DEFAULTVERSION="3.5.4"
+DEFAULTVERSION="3.6.0"
 
 # Default (=full) set of targets to build
 DEFAULTTARGETS="darwin64-x86_64-g ios64-cross-g"
